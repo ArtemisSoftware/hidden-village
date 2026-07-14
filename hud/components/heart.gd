4c82116e2b0571dcba39a332a72dd18c54eb211a
gustavo <gustavo.isaac@gmail.com>
2026-07-14 19:30:18 +0100

    Heart Fractions

--- hud/components/heart.gd
+++ hud/components/heart.gd
@@ -11,7 +11,13 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-func update(isWhole: bool) -> void:
-	if isWhole: sprite_2d.frame = 4
-	else: sprite_2d.frame = 0
+func update(fraction: int) -> void:
+	
+	match fraction:
+		0: sprite_2d.frame = 4
+		1: sprite_2d.frame = 3
+		2: sprite_2d.frame = 2
+		3: sprite_2d.frame = 1
+		4: sprite_2d.frame = 0
+
 	pass
--- hud/player/healt_hud.gd
+++ hud/player/healt_hud.gd
@@ -13,6 +13,7 @@ func _process(delta: float) -> void:
 	pass
 	
 func set_max_health(max: int) -> void:
+	max = max / 4
 	for index in range(max):
 		var heart = HEART.instantiate()
 		add_child(heart)
@@ -22,9 +23,17 @@ func set_max_health(max: int) -> void:
 func update_health(current_health: int) -> void:
 	var health = get_children()
 	
-	for index in range(current_health):
-		health[index].update(true)
+	var full_hearts = current_health / 4
+	
+	
+	for index in range(full_hearts):
+		health[index].update(4)
 		
-	for index in range(current_health, health.size()):
-		health[index].update(false)		
+	if full_hearts == health.size(): return	
+	
+	var remainder = current_health % 4
+	health[full_hearts].update(remainder)
+		
+	for index in range(full_hearts + 1, health.size()):
+		health[index].update(0)		
 	pass	
